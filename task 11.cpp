44e51140a2bee417065783dec787dfa35a2c8146
oleska <46486971+captaincod@users.noreply.github.com>
2019-12-27 15:32:25 +0800

    Add files via upload

---
+++ task 11.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <cmath>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+//Гирьки
+
+int power(int chislo, int stepen) {
+	int umnozhitel = chislo;
+	for (int i = 1; i < stepen; i++) {
+		chislo *= umnozhitel;
+	}
+	return chislo;
+}
+int number_of_digits(int chislo) {
+	int number_of_digits = 0;
+	while (chislo > 0) {
+		chislo /= 10;
+		number_of_digits++;
+	}
+	return number_of_digits;
+}
+
+int main() {
+	int dividers[] = { 10, 5, 2, 1 };
+
+	int m;
+	cout << "Enter the mass:";
+	cin >> m;
+
+	string output = "";
+
+	if (number_of_digits(m) > 0) {
+
+		while (m > 0) {
+			if (number_of_digits(m) > 1) {
+				for (int i = 0; i < 4; i++) {
+					dividers[i] *= power(10, number_of_digits(m) - 1);
+				}
+			}
+			for (int j = 0; j < 4; j++) {
+				if (m - dividers[j] >= 0) {
+					output += to_string(dividers[j]) + " ";
+					m -= dividers[j];
+				}
+			}
+			dividers[0] = 10;
+			dividers[1] = 5;
+			dividers[2] = 2;
+			dividers[3] = 1;
+		}
+		cout << "This number consists of: " << output;
+	}
+
+
+
+	else {
+		cout << "There are some problems with your input";
+	}
+
+
+	_getch();
+	return 0;
+}
---
+++ task 8.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+#include <conio.h>
+using namespace std;
+
+int main() {
+
+	//Необходимо найти количество чисел, не превышающих 10 000 000, у которых кол-во единиц в восьмеричной 
+	//и двоичной записях равно
+	
+	int max = 10000000;
+	int quantity_of_numbers = 0, binary_one_quant = 0, octal_one_quant = 0;
+
+	for (int number = 1; number <= max; number++) {
+		int binary_num = number;
+		while (binary_num > 0) {
+			if (binary_num % 2 == 1) {
+				binary_one_quant++;
+			}
+			binary_num /= 2;
+		}
+		int octal_num = number;
+		while (octal_num > 0) {
+			if (octal_num % 8 == 1) {
+				octal_one_quant++;
+			}
+			octal_num /= 8;
+		}
+
+		if ( (binary_one_quant == octal_one_quant) && binary_one_quant != 0) {
+			quantity_of_numbers++;
+		}
+		binary_one_quant = 0;
+		octal_one_quant = 0;
+	}
+
+	cout << "Quantity of numbers: " <<quantity_of_numbers;
+	_getch();
+	return 0;
+}
